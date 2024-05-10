1e8124afe77d6a3f227b9591241c14544c763cc6
unknown <anaismani13@gmail.com>
2024-05-10 02:07:19 +0200

    premier commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function nextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+setInterval(nextSlide, 3000); // Change d'image toutes les 3 secondes
